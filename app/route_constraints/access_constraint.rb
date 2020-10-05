1acc69694389a45e3e7ab00c0f26cc87f2a23872
Bill Tihen <btihen@gmail.com>
2020-10-05 19:32:17 +0200

    contraint looks nice but need debugging

---
+++ app/route_constraints/access_constraint.rb
@@ -0,0 +1,15 @@
+# https://stackoverflow.com/questions/4753871/how-can-i-redirect-a-users-home-root-path-based-on-their-role-using-devise
+class AccessConstraint
+  def initialize(*roles)
+    @roles = roles
+  end
+
+  def matches?(request)
+    @roles.include? request.env['warden'].user.try(:access_role)
+  end
+end
+
+#config/routes.rb
+# root :to => 'admin#index', :constraints => AccessConstraint.new(:admin) #matches this route when the current user is an admin
+# root :to => 'sites#index', :constraints => AccessConstraint.new(:user) #matches this route when the current user is an user
+# root :to => 'home#index' #matches this route when the above two matches don't pass
