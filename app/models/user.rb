7b259fd1a8b0e49b9b9b3773cc355b0267a8b6a0
Bill Tihen <btihen@gmail.com>
2020-10-07 12:25:03 +0200

    New user creation works

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,25 +2,30 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable, :trackable, # :registerable,
-          :recoverable, :rememberable, :validatable, 
+          :recoverable, :rememberable, :validatable,
           authentication_keys: [:login] #, :email, :username]
 
+  validates :email,       presence: true, uniqueness: true,
+                          format: { with: Devise::email_regexp }
+  validates :username,    presence: true,
+                          uniqueness: { case_sensitive: false },
+                          format: { with: /\A[a-z0-9_\-]*\z/,
+                                    message: "only lowercase letters, numbers, underscores and dashes allowed" }
+  validates :password,    presence: true, on: :create
+  validates :access_role, presence: true,
+                          inclusion: { :in => ApplicationHelper::VALID_ROLES }
+  validate  :validate_password_complexity
+
+  # https://github.com/heartcombo/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address
   attr_writer :login
 
-  # TODOS:
-  # validate email
-  # validate role in ApplicationHelper::VALID_ROLES
-  # before validation (take first part of email and parameritze for user name)
-  validates :username,  presence: true,
-                        uniqueness: { case_sensitive: false },
-                        format: { with: /\A[a-z0-9_\-]*\z/,
-                                  message: "only lowercase letters, numbers, underscores and dashes allowed" }
-
+  # https://github.com/heartcombo/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address
   def login
     @login || username || email
     # @login || self.username || self.email
   end
 
+  # https://github.com/heartcombo/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
@@ -31,5 +36,15 @@ class User < ApplicationRecord
       where(conditions.to_h).first
     end
   end
+
+  private
+
+  # https://github.com/heartcombo/devise/wiki/How-To:-Set-up-simple-password-complexity-requirements
+  def validate_password_complexity
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z]).{9,70}$/
+
+    errors.add :password, 'Complexity requirement not met. Length should be 9-70 characters long and includes: 1 uppercase and 1 lowercase.'
+  end
+
 
 end
