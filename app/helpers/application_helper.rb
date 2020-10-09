3e558330b88f2273c5c6814f154446aad03a3895
Bill Tihen <btihen@gmail.com>
2020-10-09 21:03:39 +0200

    disable old permission settings - so they can't return

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
 
   USER_ROLES_AND_PERMISSIONS = [
-    {role: "trustee", permissions: "Full access (Depricated)"},
-    {role: "manager", permissions: "Full access"},
-    {role: "planner", permissions: "Create/edit booking"},
-    {role: "umdze",   permissions: "Update (Depricated)"},
-    {role: "host",    permissions: "Update Umdze/Koord."},
-    {role: "viewer",  permissions: "View only"},
-    {role: "member",  permissions: "Read only (Depricated)"},
+    {role: "manager", permissions: "Full Manager Access"},
+    {role: "planner", permissions: "Create/Edit bookings"},
+    {role: "host",    permissions: "Update Coordinators"},
+    {role: "viewer",  permissions: "View Calendar Plan"},
+    # {role: "umdze",   permissions: "Update (Depricated)"},
+    # {role: "trustee", permissions: "Full access (Depricated)"},
+    # {role: "member",  permissions: "Read only (Depricated)"},
   ]
 
   VALID_ROLES = USER_ROLES_AND_PERMISSIONS.map { |rp| rp[:role] }
