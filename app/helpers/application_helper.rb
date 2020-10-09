f4ec3befcb33895b22f01cadfea5e38c608f3c62
Bill Tihen <btihen@gmail.com>
2020-10-09 20:22:18 +0200

    Opps missed permission listing update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 
   USER_ROLES_AND_PERMISSIONS = [
-    {role: "trustee", permissions: "Full access Depricated"},
-    {role: "manager", permissions: "Full access (Manager)"},
-    {role: "planner", permissions: "Create/edit Booking"},
-    {role: "umdze",   permissions: "Update Umdze/Koord."},
+    {role: "trustee", permissions: "Full access (Depricated)"},
+    {role: "manager", permissions: "Full access"},
+    {role: "planner", permissions: "Create/edit booking"},
+    {role: "umdze",   permissions: "Update (Depricated)"},
+    {role: "host",    permissions: "Update Umdze/Koord."},
     {role: "viewer",  permissions: "View only"},
     {role: "member",  permissions: "Read only (Depricated)"},
   ]
